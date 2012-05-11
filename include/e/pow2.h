871e53df9c0b839b72beb9145cd8cbe57ab0066a
Robert Escriva <me@robescriva.com>
2012-05-10 22:46:31 -0400

    Add a function to compute next-highest pow2.

---
+++ include/e/pow2.h
@@ -0,0 +1,63 @@
+// Copyright (c) 2012, Robert Escriva
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of this project nor the names of its contributors may
+//       be used to endorse or promote products derived from this software
+//       without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef e_pow2_h_
+#define e_pow2_h_
+
+namespace e
+{
+
+// Compute the next highest power of two
+
+uint32_t
+next_pow2(uint32_t in)
+{
+    --in;
+    in = (in >> 1) | in;
+    in = (in >> 4) | in;
+    in = (in >> 8) | in;
+    in = (in >> 16) | in;
+    ++in;
+    return in;
+}
+
+uint64_t
+next_pow2(uint64_t in)
+{
+    --in;
+    in = (in >> 1) | in;
+    in = (in >> 4) | in;
+    in = (in >> 8) | in;
+    in = (in >> 16) | in;
+    in = (in >> 32) | in;
+    ++in;
+    return in;
+}
+
+} // namespace e
+
+#endif // e_pow2_h_
