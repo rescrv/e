62fb45e5c18b829d05ec13f69c820a4baff602d1
Robert Escriva <robert@rescrv.net>
2016-08-18 12:18:30 -0400

    Add stdlib include for slices

--- slice.cc
+++ slice.cc
@@ -25,6 +25,9 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+// C
+#include <stdlib.h>
+
 // STL
 #include <iomanip>
 #include <sstream>
